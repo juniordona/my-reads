f27cb3abeb5d7cc37877b384ea5138883994aa8f
Júnior Donã <valdemirdonajr@gmail.com>
2018-11-06 18:05:15 -0200

    mover livro

--- src/App.js
+++ src/App.js
@@ -7,61 +7,60 @@ import Shelf from './Componentes/Shelf'
 class BooksApp extends React.Component {
   state = {
     showSearchPage: false,
-    books : []
+    books: []
   };
 
 
-  componentDidMount = () =>{
+  componentDidMount = () => {
     BooksAPI.getAll().then((books) => {
       this.setState({
-          books: books
+        books: books
       });
     })
   }
 
   mover = (book, newValue) => {
+    console.log(book.props.book.shelf);
+    console.log(book.props.book);
+    console.log(newValue);
 
     book.props.book.shelf = newValue;
 
-    this.setState( (state) => ({
-        books: state.books.filter( (b) => b.id !== book.props.book.id).concat([book.props.book])
+    this.setState((state) => ({
+      books: state.books.filter((b) => b.id !== book.props.book.id).concat([book.props.book])
     }))
 
     BooksAPI.update(book.props.book, newValue);
 
-}
-
+  }
 
   render() {
     return (
       <div className="app">
-          
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-                
-                  <Shelf
-                        title="Currently Reading"
-                        cat="currentlyReading"
-                        books={this.state.books.filter(books => books.shelf === 'currentlyReading')}
-                        onBook={this.mover}
-                    />
-                    <Shelf
-                        title="Want to Read"
-                        cat="wantToRead"
-                        books={this.state.books.filter(books => books.shelf === 'wantToRead')}
-                        onBook={this.mover}
-                    />
-                    <Shelf
-                        title="Read"
-                        cat="read"
-                        books={this.state.books.filter(books => books.shelf === 'read')}
-                        onBook={this.mover}
-                    />
-              
-            </div>
+
+        <div className="list-books">
+
+          <Shelf
+            title="Currently Reading"
+            cat="currentlyReading"
+            books={this.state.books.filter(books => books.shelf === 'currentlyReading')}
+            onBook={this.mover}
+          />
+          <Shelf
+            title="Want to Read"
+            cat="wantToRead"
+            books={this.state.books.filter(books => books.shelf === 'wantToRead')}
+            onBook={this.mover}
+          />
+          <Shelf
+            title="Read"
+            cat="read"
+            books={this.state.books.filter(books => books.shelf === 'read')}
+            onBook={this.mover}
+          />
+
         </div>
+      </div>
     )
   }
 }
