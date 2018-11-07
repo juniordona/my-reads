28113114952ca7a9f803750f984aef802399b93a
Júnior Donã <valdemirdonajr@gmail.com>
2018-11-07 16:39:50 -0200

    arrumando thumnail vazio e marcação de nono

--- src/Componentes/Book.js
+++ src/Componentes/Book.js
@@ -13,11 +13,11 @@ class Book extends Component {
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193 }}>
-                        <img alt="" src={this.props.book.imageLinks.thumbnail} style={{ width: 128, height: 193 }}/>
+                        <img alt="" src={this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''} style={{ width: 128, height: 193 }}/>
                     </div>
                     <div className="book-shelf-changer">
-                        <select onChange={this.handleChange} defaultValue={this.props.book.shelf}>
-                            <option value="none" disabled>Move to...</option>
+                        <select onChange={this.handleChange} defaultValue={this.props.book.shelf ? this.props.book.shelf : 'none'}>
+                            <option value="" disabled>Move to...</option>
                             <option value="currentlyReading" >Currently Reading</option>
                             <option value="wantToRead" >Want to Read</option>
                             <option value="read" >Read</option>
